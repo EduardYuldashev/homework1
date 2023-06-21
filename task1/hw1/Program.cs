de58df2bae3f1a8cbc8cc83b119fe17e3082f997
Eduard_Yuldashev <4akaaaa@gmail.com>
2023-06-21 20:52:31 +0300

    done1

--- task1/hw1/Program.cs
+++ task1/hw1/Program.cs
@@ -1,23 +1,2 @@
-﻿Console.Clear();
-int max = 0;
-int min = 0;
-
-Console.Write("Введите первое число:");
-string userInput1 = Console.ReadLine() ?? "";
-int aNumber = int.Parse(userInput1);
-
-Console.Write("Введите второе число:");
-string userInput2 = Console.ReadLine() ?? "";
-int bNumber = int.Parse(userInput2);
-
-if(aNumber > bNumber)
-{
-    max = aNumber;
-    min = bNumber;
-}
-else
-{
-    max = bNumber;
-    min = aNumber;
-}
-Console.WriteLine ($"max={max}, min={min}");
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
